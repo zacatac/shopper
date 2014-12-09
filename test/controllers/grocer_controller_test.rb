ac46a01d662fee62fd02604b730d90b1f102678b
zacatac <field.zackery@gmail.com>
2014-12-09 04:34:23 -0800

    add test fixtures; no tests implemented

---
+++ test/controllers/grocer_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GrocerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/grocer_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class GrocerHelperTest < ActionView::TestCase
+end
